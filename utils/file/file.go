0b83ba6eebc9c3af2db864a74adb404befbed9eb
zhangxiong <zhangxiong@tap4fun.com>
2021-08-25 12:05:51 +0800

    file

--- utils/file/file.go
+++ utils/file/file.go
@@ -1,16 +1,119 @@
 package file
 
-import "io/ioutil"
+import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+)
 
-//check the given path exist
+func TarDeCompress(sourceFile, targetPath string, needCover bool) error {
+	if []rune(targetPath)[len([]rune(targetPath)) - 1] != '/' {
+		targetPath = targetPath + "/"
+	}
+
+	f, err := os.Open(sourceFile)
+	if err != nil {
+		return err
+	}
+	defer func() {_ = f.Close()}()
+
+	g, err := gzip.NewReader(f)
+	if err != nil {
+		return err
+	}
+	defer func() {_ = g.Close()}()
+
+	tr := tar.NewReader(g)
+	for {
+		header, err := tr.Next()
+		if err != nil{
+			if err == io.EOF{
+				break
+			}else{
+				return err
+			}
+		}
+
+		filename := targetPath + header.Name
+		isExist := Exist(filename)
+		if isExist == false {
+			if !IsFile(filename) {
+				if err := os.MkdirAll(filename, 0755); err != nil{
+					return err
+				}
+				log.Printf("decompress add folder : %s", filename)
+			}else{
+				if err := createFile(filename, tr); err != nil{
+					return err
+				}
+				log.Printf("decompress add file : %s", filename)
+			}
+		}else{
+			if needCover == true {
+				if !IsFile(filename) {
+					//清空文件夹
+					if err := clearFolder(filename); err != nil{
+						log.Printf("decompress clear folder's file : %s", filename)
+						return err
+					}
+				}else{
+					if err := createFile(filename, tr); err != nil{
+						return err
+					}
+					log.Printf("decompress cover file : %s", filename)
+				}
+			}else{
+				//不需要覆盖的情况无需操作
+			}
+		}
+	}
+	return nil
+}
+
+func createFile(filename string, tr io.Reader) error {
+	file, err := os.Create(filename)
+	if err != nil{
+		return err
+	}
+	if _, err := io.Copy(file, tr); err != nil {
+		return err
+	}
+	return nil
+}
+
+func clearFolder(folder string)error {
+	dir, err := ioutil.ReadDir(folder)
+	if err != nil {
+		return err
+	}
+	for _, d := range dir{
+		os.RemoveAll(path.Join([]string{folder, d.Name()}...))
+	}
+	return nil
+}
+
+// Exist check the given path exist or not
 func Exist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
 	return true
 }
 
-//check the given path is file or dir
-//@return bool | true means file, false means dir or not exist
-func IsFile() bool {
-
+// IsFile check the given path is a file or not
+func IsFile(path string) bool{
+	if path == filepath.Dir(path) || path == filepath.Dir(path) + "/"{
+		return false
+	}
 	return true
 }
 
